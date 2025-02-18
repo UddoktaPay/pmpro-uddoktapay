50aa6cca18c753c9ccd57a0795b40561831bb509
Rasel Islam Rafi <31556372+rtraselbd@users.noreply.github.com>
2025-02-18 21:39:45 +0600

    Update pmpro-uddoktapay.php

--- pmpro-uddoktapay.php
+++ pmpro-uddoktapay.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Paid Memberships Pro - UddoktaPay Gateway
  * Plugin URI: https://github.com/UddoktaPay/pmpro-uddoktapay
- * Description: PMPro Gateway integration for UddoktaPay
+ * Description: UddoktaPay Gateway integration for Paid Memberships Pro
  * Version: 1.0.1
  * Requires at least: 6.2
  * Requires PHP: 7.4
